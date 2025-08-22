dff7590c82c00d85a17d188add6b162e40bed445
Flooooooooooorian <florian.weber@neuefische.de>
2025-08-22 16:42:47 +0200

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,44 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hallo Welt");
+        System.out.println("Tschüüs");
+
+
+        //Datentyp variableNamen = value;
+        //String, double, float, byte, short, int, long, boolean, char,
+        // Text, Kommazahl(groß), Kommazahl(klein), Ganzzahlen(), Ganzzahlen(groß), true/false, '!'
+
+        String myName = "Florian, 29 Jahre alt, wohnt in Berlin";
+        double myAge = 29.1;
+        int myValue = 4;
+
+        double myself = myValue + myAge;
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(myself);
+
+        System.out.println(3 + 4);
+        System.out.println(3 - 4);
+        System.out.println(3 / 4);
+        System.out.println(3 * 4);
+
+        boolean myResult = 3 > 5.5;
+        boolean result = false;
+
+        System.out.println(myResult);
+        System.out.println(3 < 5);
+        System.out.println(3 == 5);
+        System.out.println(3 != 5);
+        System.out.println(3 >= 5);
+        System.out.println(3 <= 5);
+
+        System.out.println("Ein und: " + (true && true));
+        System.out.println("Ein oder: " + (true || false));
+
+        System.out.println(16 % 5);
+        System.out.println(584684636 % 2);
+    }
+}
---
+++ src/main/java/Methoden.java
@@ -0,0 +1,74 @@
+public class Methoden {
+
+    public static void main(String[] args) {
+
+        String result1 = returnHello();
+//        int result2 = myMethod();
+        String result3 = myMethod("string");
+
+        int result = 2;
+
+        String intValue = String.valueOf(result);
+
+        int value = Integer.parseInt("2");
+
+        System.out.println("Int value: " + value);
+
+        if (result > 0) {
+            System.out.println("Ja");
+        }
+
+        switch (result) {
+            case 0: {
+                System.out.println("Die Varible ist 0");
+                break;
+            }
+            case 1: {
+                System.out.println("Die Varible ist 1");
+                break;
+            }
+            case 2: {
+                System.out.println("Die Varible ist 2");
+                break;
+            }
+            case 3: {
+                System.out.println("Die Varible ist 3");
+                break;
+            }
+            case 4: {
+                System.out.println("Die Varible ist 4");
+                break;
+            }
+            case 5: {
+                System.out.println("Die Varible ist 5");
+                break;
+            }
+        }
+
+
+        String nameAndSize = ("Florian");
+
+        System.out.println(nameAndSize);
+
+    }
+
+    public static String returnHello() {
+        String hallo = "Hallo";
+        return hallo;
+    }
+
+    public static String myMethod(String name) {
+        String hallo = "Hallo";
+        int length = hallo.length();
+
+        return hallo+length;
+    }
+
+
+    public static boolean isBiggerThan10(int number) {
+        return number > 10;
+    }
+
+
+
+}
